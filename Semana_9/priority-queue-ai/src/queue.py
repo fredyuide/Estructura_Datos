a5eac0b0553f5dc59410833dee4fb8749a4c302e
fredyuide <frguamango@uide.edu.ec>
2025-01-15 22:20:14 -0500

    Update queue.py

--- Semana_9/priority-queue-ai/src/queue.py
+++ Semana_9/priority-queue-ai/src/queue.py
@@ -1,1 +1,12 @@
+from queue import PriorityQueue
+
+class PriorityQueueWithAI(PriorityQueue):
+    def __init__(self):
+        super().__init__()
+
+    def enqueue(self, item, priority):
+        self.put((priority, item))
+
+    def dequeue(self):
+        return self.get()
 
